1a1a63b3f7be142e93a4fc47a2537efa8c8cf32b
yakovbenalex <yakovbenalex@mail.ru>
2022-02-10 01:09:35 +0300

    tsk0001 add main script funtional

---
+++ Convert time seconds count to normal format.py
@@ -0,0 +1,25 @@
+import sys
+
+secCount = 0  # initial value
+
+# get seonds count from cmd or user input
+if len(sys.argv) > 1:
+    try:
+        secCount = int(sys.argv[1])
+    except ValueError:
+        print("Input is not a number")
+else:
+    secCount = int(input("enter number of second\n"))
+
+# convert seconds count to format hh:mm:ss
+if secCount < 3600:
+    minutes = secCount // 60
+    seconds = secCount % 60
+    time = "%d:%02d" % (minutes, seconds)
+else:
+    hours = secCount // 3600
+    minutes = (secCount - 3600*(secCount // 3600)) // 60
+    seconds = (secCount - 3600*(secCount // 3600)) % 60
+    time = "%d:%02d:%02d" % (hours, minutes, seconds)
+
+print(time)
